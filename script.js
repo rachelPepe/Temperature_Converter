62ea93471b2066ed346e92ec979862275ef564f9
rachelpepe <rachelmpepe@gmail.com>
2024-10-04 16:57:53 -0700

    committing completed project

--- script.js
+++ script.js
@@ -4,11 +4,57 @@ window.addEventListener("DOMContentLoaded", domLoaded);
 /* calls event listener to register click event for convert button, calls
 setImage function and displays error message if a number is not input */
 function domLoaded() {
+    const cInput = document.getElementById("cInput");
+    const fInput = document.getElementById("fInput");
+    const convertButton = document.getElementById("convertButton");
+    const weatherImage = document.getElementById("weatherImage");
+    const errorMessage = document.getElementById("errorMessage");
+
+    // event listeners to prevent celcius and fahrenheit from being input at the same time
+    cInput.addEventListener("input", function() {
+        fInput.value = "";
+    })
+    fInput.addEventListener("input", function() {
+        cInput.value= "";
+    })
+
+    // convert buttons event handler
+    convertButton.addEventListener("click", function() {
+        var celcius = parseFloat(cInput.value)
+        var fahrenheit = parseFloat(fInput.value)
+
+        if(!isNaN(fahrenheit)){
+            cInput.value = convertFtoC(fahrenheit).toFixed(0);
+            setImage(fahrenheit);
+            errorMessage.textContent = ""
+        }
+        else if (!isNaN(celcius)){
+            fInput.value = convertCtoF(celcius).toFixed(0);
+            setImage(convertCtoF(celcius))
+            errorMessage.textContent = ""
+        }
+        else if (isNaN(celcius) || isNaN(fahrenheit)) {
+            errorMessage.textContent = `${cInput.value || fInput.value} is not a number`
+        }
+    })
+
 
 }
 
-function convertDegrees (event) {
-    //
+// function to change image according to fahrenheit degree
+function setImage(degree) {
+    if (degree < 32) {
+        weatherImage.src = "images/cold.png";
+        weatherImage.alt = "cold";
+    }
+    else if (degree >= 32 && degree <= 50) {
+        weatherImage.src = "images/cool.png";
+        weatherImage.alt = "cool";
+    }
+    else {
+        weatherImage.src = "images/warm.png";
+        weatherImage.alt = "warm";
+    }
 }
 
 function convertFtoC(fahrenheitDegree) {
@@ -20,3 +66,5 @@ function convertCtoF(celciusDegree) {
     // returns celcius conversion
     return celciusDegree * 9/5 + 32
 }
+
+
